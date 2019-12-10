9813e0128d8eb8eeba35d02dbbba79d7f80633bb
artseven <yabkevich.artsiom.09@gmail.com>
2019-12-10 16:15:25 -0500

    added store and store modules

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -6,17 +6,9 @@
 
 <script>
 import Stock from './Stock.vue'
+import stocks from '../../data/stocks';
+
 export default {
-	data() {
-		return {
-			stocks : [
-				{ id: 1, name: 'BMW', price: 110},
-				{ id: 2, name: 'Google', price: 200},
-				{ id: 3, name: 'Apple', price: 250},
-				{ id: 4, name: 'Twitter', price: 8}
-			]
-		}
-	},
 	components: {
 		Stock
 	}
