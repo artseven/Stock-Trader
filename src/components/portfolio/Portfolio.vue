6b001ba3d7627fd89274037b065d148771af2cd2
artseven <yabkevich.artsiom.09@gmail.com>
2019-12-11 12:36:37 -0500

    working version

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,39 +1,25 @@
 <template>
-  <div class="col-sm-6 col-md-4">
-    <div class="panel panel-success">
-      <div class="panel-heading">
-        <h3 class="panel-title">
-          {{ stock.name }}
-          <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
-        </h3>
-      </div>
-      <div class="panel-body">
-        <div class="pull-left">
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Quantity"
-            v-model="quantity"
-          />
-        </div>
-        <div class="pull-right">
-          <button class="btn btn-success" @click="buyStock" :disabled="quantity <= 0 || Number.isInteger(quantity)">Sell</button>
-        </div>
-      </div>
-    </div>
+  <div>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock"></Stock>
   </div>
 </template>
 
 <script>
-export default {
-  props: ["stock"],
-  data() {
-    return {
-      quantity: 0
-    };
-	},
+import {mapGetters} from 'vuex';
+import Stock from './Stock.vue';
 
-};
+export default {
+  computed: {
+    ...mapGetters({
+      stocks: 'stockPortfolio'
+    })
+  },
+  components: {
+    Stock
+  }
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
